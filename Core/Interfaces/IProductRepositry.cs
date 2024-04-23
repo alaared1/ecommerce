f732a3670220c22b2627746a2da21cfb883203f0
Alaa Eddine Bouhadjeb <alaa.bouhadjeb@avanade.com>
2024-04-23 20:07:42 +0200

    Add decoupling

---
+++ Core/Interfaces/IProductRepositry.cs
@@ -0,0 +1,12 @@
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepositry
+    {
+         Task<Product> GetProductByIdAsync(int id);
+         Task<IReadOnlyList<Product>> GetProductsAsync();
+         Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+         Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
+    }
+}

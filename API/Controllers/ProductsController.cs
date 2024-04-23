91236092c1f138ee79a6e22e4a576f070a16c0c5
Alaa Eddine Bouhadjeb <alaa.bouhadjeb@avanade.com>
2024-04-23 17:55:51 +0200

    Setup Core and Infrastructure

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,31 +1,44 @@
-using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
-    { 
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context) 
+    {
+        private readonly IProductRepositry _repository;
+        public ProductsController(IProductRepositry repository)
         {
-            _context = context;
+            _repository = repository;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
-            return products;
+            var products = await _repository.GetProductsAsync();
+            return Ok(products);
         }
         //Just a comment here to test the font
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repository.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
+        {
+            var brands = await _repository.GetProductBrandsAsync();
+            return Ok(brands);
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<List<ProductType>>> GetProductTypes()
+        {
+            var types = await _repository.GetProductTypesAsync();
+            return Ok(types);
         }
     }
 }
